9b88bf260cf523f7330ab6a4c3e0279c06cc1229
unknown <drewradley@gmail.com>
2018-08-09 19:04:53 -0700

    Trivia Game Challenge

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -209,14 +209,14 @@ function AnswerScreen()
     if(thisAnswer==0)
     {
         $(".alert-success").text("Correct");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=correct&api_key=dc6zaTOxFJmzC&limit=25";
+        queryURL = "http://api.giphy.com/v1/gifs/search?q=correct&api_key=dc6zaTOxFJmzC&limit=25";
         getGIF();
     }
 
     if(thisAnswer>0)
     {
         $(".alert-success").text("Wrong");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=wrong&api_key=dc6zaTOxFJmzC&limit=25";
+        queryURL = "http://api.giphy.com/v1/gifs/search?q=wrong&api_key=dc6zaTOxFJmzC&limit=25";
         getGIF();
     }
     function getGIF()
